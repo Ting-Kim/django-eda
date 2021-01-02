443ddb5d8cdf54429f6061008e2d5eb9eab726d7
Ting-Kim <taeho916@gmail.com>
2021-01-03 08:56:31 +0900

    collectstatic

--- myeda/myeda/settings.py
+++ myeda/myeda/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_DIR = os.path.join(BASE_DIR, 'eda', 'static', 'eda')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'eda', 'static', 'eda'),
+    STATIC_DIR,
 ]
